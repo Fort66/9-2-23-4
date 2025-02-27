9164ba8dbdd60429f2e4197ce4ff699b538a9ddc
Oleg <mamonov.1966@yandex.ru>
2025-02-27 13:10:31 +0300

    Add explosion effect for enemies guard collision

--- functions/function_shots_collision.py
+++ functions/function_shots_collision.py
@@ -16,4 +16,14 @@ def player_guard_collision(self):
             obj=self,
             angle=self.angle,
             )
-        self.kill()
+
+def enemies_guard_collision(self):
+    if spritecollideany(self, sprite_groups.enemies_guard_group) and self.owner not in sprite_groups.enemies_group:
+        explosion = Explosion(
+            dir_path='images/explosion/rocket1_expl',
+            speed_frame=.01,
+            scale_value=(.5, .5),
+            loops=1,
+            obj=self,
+            angle=self.angle,
+            )
